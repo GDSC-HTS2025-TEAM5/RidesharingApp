0b4545bba116bae0467975ad7385a78dfe5353f1
daniel06smith <2006smithdaniel@gmail.com>
2025-04-07 22:19:52 -0700

    Requested rides are now appearing and can be accepted in DriverDashboard.

--- rideshare/rides/urls.py
+++ rideshare/rides/urls.py
@@ -4,6 +4,6 @@ from .views import CreateRideView, RideListView, AcceptRideView
 
 urlpatterns = [
     path('create/', CreateRideView.as_view(), name='create-ride'),
-    path('list/', RideListView.as_view(), name='ride-list'),
+    path('', RideListView.as_view(), name='ride-list'),
     path('accept/<int:pk>/', AcceptRideView.as_view(), name='accept-ride'),
 ]
--- rideshare/rideshare/urls.py
+++ rideshare/rideshare/urls.py
@@ -1,8 +1,9 @@
+# rideshare/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/rides/', include('rides.urls')),
+    path('api/rides/', include('rides.urls')),  # ← MAKE SURE THIS LINE EXISTS
 ]
