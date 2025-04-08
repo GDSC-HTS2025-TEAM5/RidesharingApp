c8ce1e2075ef3f77b6a1cd9bf5f041b363d1b9e8
daniel06smith <2006smithdaniel@gmail.com>
2025-04-07 22:04:38 -0700

    /s

--- rideshare/rides/serializers.py
+++ rideshare/rides/serializers.py
@@ -3,7 +3,14 @@ from rest_framework import serializers
 from .models import Ride
 
 class RideSerializer(serializers.ModelSerializer):
+    user_first_name = serializers.CharField(source='user.first_name', read_only=True)
+    user_last_name = serializers.CharField(source='user.last_name', read_only=True)
+
     class Meta:
         model = Ride
-        fields = ['id', 'user', 'start_location', 'end_location', 'departure_time', 'created_at', 'accepted_by']
+        fields = [
+            'id', 'user', 'start_location', 'end_location',
+            'departure_time', 'created_at', 'accepted_by',
+            'user_first_name', 'user_last_name',
+        ]
         read_only_fields = ['id', 'user', 'created_at', 'accepted_by']
